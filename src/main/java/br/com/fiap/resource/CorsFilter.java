0ba75b28c3c7b41d4eb85594df5c219d719783c9
devbelasp <isa.bela360@hotmail.com>
2025-11-19 16:25:29 -0300

    teste

--- src/main/java/br/com/fiap/resource/CorsFilter.java
+++ src/main/java/br/com/fiap/resource/CorsFilter.java
@@ -4,27 +4,19 @@ import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
 import jakarta.ws.rs.ext.Provider;
-
 import java.io.IOException;
 
-/**
- * Filtro CORS (Cross-Origin Resource Sharing).
- * Adiciona cabeçalhos de permissão para que aplicações front-end (de domínios diferentes)
- * possam consumir a API Java, resolvendo problemas de segurança de navegador.
- */
 @Provider
 public class CorsFilter implements ContainerResponseFilter {
 
-    /**
-     * Adiciona os cabeçalhos HTTP necessários para permitir requisições de qualquer origem.
-     */
     @Override
     public void filter(ContainerRequestContext request,
                        ContainerResponseContext response) throws IOException {
-        response.getHeaders().add("Access-Control-Allow-Origin", "*");
+
+        response.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:5173");
+
         response.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
         response.getHeaders().add("Access-Control-Allow-Credentials", "true");
         response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     }
-
 }
